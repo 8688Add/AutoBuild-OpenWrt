89badc3eb0793f0eb3b21f76ed1dad992e313af8
8688Add <42204083+8688Add@users.noreply.github.com>
2020-01-12 08:55:31 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#git clone https://github.com/brokeld/luci-app-koolproxyR.git package/luci-app-koolproxyR
+
+#git clone https://github.com/Leo-Jo-My/luci-app-vssr.git package/luci-app-vssr
