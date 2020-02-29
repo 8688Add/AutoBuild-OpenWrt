1c4650feeedb8d4f40fe0e4c22fc8a66e1aa18ca
8688Add <42204083+8688Add@users.noreply.github.com>
2020-02-29 17:16:15 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,25 +9,15 @@
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #git clone https://github.com/brokeld/luci-app-koolproxyR.git package/luci-app-koolproxyR
-
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-mia
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-timewol
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-webrestriction
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-weburl
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-passwall
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-trojan-server
-
-git clone https://github.com/Lienol/openwrt-package.git package/luci-theme-argon-dark-mod
-
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-mia
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-timewol
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-webrestriction
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-control-weburl
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-passwall
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-app-trojan-server
+#git clone https://github.com/Lienol/openwrt-package.git package/luci-theme-argon-dark-mod
 #git clone https://github.com/Lienol/openwrt-package.git package/luci-app-fileassistant
-
 #git clone https://github.com/project-openwrt/luci-app-233ddns.git package/luci-app-233ddns
 
 
